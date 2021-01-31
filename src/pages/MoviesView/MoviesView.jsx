572e078a2d9199c4d348372f5536eede3d61eef4
Viktoriia Melanchenko <victoriamelanchenko@gmail.com>
2021-01-31 18:16:13 +0200

    add URLSearchParams for query string

--- src/pages/MoviesView/MoviesView.jsx
+++ src/pages/MoviesView/MoviesView.jsx
@@ -1,22 +1,31 @@
 import SearchBar from '../../components/SearchBar/SearchBar';
 import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 import FetchMovieRequest from '../../components/FetchMovieRequest/FetchMovieRequest';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function MoviesView() {
+  const history = useHistory();
+  const location = useLocation();
   const [query, setQuery] = useState('');
+
+  const searchQuery = new URLSearchParams(location.search).get('query') ?? '';
 
   const handleFormSubmit = (query) => {
     setQuery(query);
+    history.push({
+      ...location,
+      search: `query=${query}`,
+    });
   };
 
   return (
     <>
       <SearchBar onSubmit={handleFormSubmit} />
       <ToastContainer autoClose={3000} />
-      <FetchMovieRequest query={query} />
+      <FetchMovieRequest query={searchQuery} />
     </>
   );
 }
